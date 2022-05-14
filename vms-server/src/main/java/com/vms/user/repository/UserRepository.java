f684bde548dee11dfe122b61bc297d428234ae67
Mikheil Mukhiguli <mimukh18@freeuni.edu.ge>
2022-05-14 20:17:18 +0400

    Client-UI: Added swagger configuration. Paging on users page.

--- vms-server/src/main/java/com/vms/user/repository/UserRepository.java
+++ vms-server/src/main/java/com/vms/user/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.vms.user.repository;
 
 import com.vms.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 }
--- vms-ui/src/main/java/com/vms/group/GroupController.java
+++ vms-ui/src/main/java/com/vms/group/GroupController.java
@@ -2,6 +2,7 @@ package com.vms.group;
 
 import com.vms.group.dto.GroupDTO;
 import com.vms.group.mapper.GroupMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/groups")
+@Tag(name = "Groups")
 public class GroupController {
 
     @Autowired
