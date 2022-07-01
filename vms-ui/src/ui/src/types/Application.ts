6917d6a7ffe5cbf439d764c1e934a12a6ae1a95e
gkhelo <gkhel18@freeuni.edu.ge>
2022-07-01 14:10:33 +0400

    Added application type

---
+++ vms-ui/src/ui/src/types/Application.ts
@@ -0,0 +1,9 @@
+export type Application = {
+  id?: number;
+  createTime?: string | null;
+  updateTime?: string | null;
+  version?: number;
+  name?: string | null;
+  companyId?: number | null;
+  vendorId?: number | null;
+};
